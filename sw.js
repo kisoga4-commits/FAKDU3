6491a6980d0081ddaffab04b26adb1ca9a756a83
kisoga4-commits <kisoga4@gmail.com>
2026-03-22 05:33:08 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js - Service Worker (FAKDU v9.46)
 
-const SW_VERSION = '9.46.7';
+const SW_VERSION = '9.46.8';
 const CACHE_NAME = `fakdu-cache-v${SW_VERSION}`;
 const META_CACHE_NAME = `fakdu-cache-meta-v${SW_VERSION}`;
 const CACHE_MAX_AGE_MS = 60 * 24 * 60 * 60 * 1000;
